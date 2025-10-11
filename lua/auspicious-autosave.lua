0e118049b2cb5a5c17d9f2210dce780d38873290
Daniel Kuettel <dkuettel@gmail.com>
2025-10-11 13:53:46 +0200

    another try to have the public interface easy to see at the top

--- lua/auspicious-autosave.lua
+++ lua/auspicious-autosave.lua
@@ -1,21 +1,93 @@
--- see bottom of file for the public interface
+local M = {} -- public interface
+local m = {} -- private
+
+--- public interface ------------------------------------------
+
+function M.setup()
+    -- controls CursorHold and CursorHoldI events
+    -- (idle time before they are triggered in milliseconds)
+    -- might want to check that it's not set to a longer value again later
+    vim.opt.updatetime = 500
+
+    -- checks if files have changed on disk, especially on FocusGained
+    vim.opt.autoread = true
+
+    -- in case anything slips by the events
+    -- (will write when windows change buffers in various ways)
+    vim.opt.autowrite = true
+    vim.opt.autowriteall = true
+
+    m.register_events()
+end
+
+---@param buf? integer
+---@return boolean
+function M.enabled(buf)
+    buf = m.real_buf(buf)
+    return m.enableds[buf]
+end
+
+---@param v boolean
+---@param buf? integer
+function M.set(v, buf)
+    buf = m.real_buf(buf)
+    m.enableds[buf] = v
+end
+
+---@param buf? integer
+function M.enable(buf)
+    M.set(true, buf)
+end
+
+---@param buf? integer
+function M.disable(buf)
+    M.set(false, buf)
+end
+
+---@param buf? integer
+---@return boolean
+function M.toggle(buf)
+    buf = m.real_buf(buf)
+    m.enableds[buf] = not m.enableds[buf]
+    return m.enableds[buf]
+end
+
+---@alias State "readonly" | "locked" | "autosave" |  "modified" | "saved" | "disappeared"
+-- readonly     read-only file, no autosaving
+-- locked       locked file (the file might still be writeable), no autosaving
+-- autosave     normal autosave operation
+-- modified     no autosaving, but the file has been modified
+-- saved        no autosaving, the file has been saved and not changed since
+-- disappeared  used to autosave, but then the file disappeared
+--              wont autosave anymore, as that would recreate a file that was potentially removed on purpose
+--              a manual :w or :bd can resolve the situation
+
+---@param buf? integer
+---@return State
+function M.state(buf)
+    buf = m.real_buf(buf)
+    m.update_state(buf)
+    return m.states[buf]
+end
+
+--- private ------------------------------------------
 
 ---@type boolean[]
-local enableds = {}
+m.enableds = {}
 
 ---@type State[]
-local states = {}
+m.states = {}
 
 ---@param buf integer
-local function update_state(buf)
+function m.update_state(buf)
     assert(buf > 0)
 
-    local enabled = enableds[buf]
+    local enabled = m.enableds[buf]
     if enabled == nil then
         enabled = true
     end
 
-    local state = states[buf]
+    local state = m.states[buf]
     local bo = vim.bo[buf]
     local name = vim.api.nvim_buf_get_name(buf)
 
@@ -60,18 +132,18 @@ local function update_state(buf)
         end
     end
 
-    enableds[buf] = enabled
-    states[buf] = state
+    m.enableds[buf] = enabled
+    m.states[buf] = state
 end
 
 ---@param context vim.api.keyset.create_autocmd.callback_args
-local function on_buffer_event(context)
+function m.on_buffer_event(context)
     -- NOTE we reevaluate the situation every time again
     -- we dont use vim.b.autosave from last time, because things could change
     -- vim.b.autosave is mainly set so that it can be used for the status line
-    update_state(context.buf)
+    m.update_state(context.buf)
 
-    if states[context.buf] == "autosave" then
+    if m.states[context.buf] == "autosave" then
         -- :update only saves if the file has been modified, no-op otherwise
         -- :silent prevents "xyz bytes write" from popping up everytime, but also hides error messages
         -- :lockmarks prevents marks like [ and ] from changing
@@ -80,13 +152,13 @@ local function on_buffer_event(context)
 end
 
 ---@param context vim.api.keyset.create_autocmd.callback_args
-local function on_focus_gained(context) ---@diagnostic disable-line: unused-local
+function m.on_focus_gained(context) ---@diagnostic disable-line: unused-local
     local bufs = vim.api.nvim_list_bufs()
     local missing = {}
     for _, buf in ipairs(bufs) do
         if vim.api.nvim_buf_is_loaded(buf) then
-            update_state(buf)
-            if states[buf] == "disappeared" then
+            m.update_state(buf)
+            if m.states[buf] == "disappeared" then
                 table.insert(missing, buf)
             end
         end
@@ -96,7 +168,7 @@ local function on_focus_gained(context) ---@diagnostic disable-line: unused-loca
     end
 end
 
-local function register_events()
+function m.register_events()
     -- interesting events:
     --   InsertLeave, TextChanged, CursorHold
     --   TextChangedI, CursorHoldI, but TextChangedI is on every keystroke
@@ -110,7 +182,7 @@ local function register_events()
         {
             group = group,
             desc = "auspicious-autosave buffer",
-            callback = on_buffer_event,
+            callback = m.on_buffer_event,
             nested = true, -- NOTE otherwise we dont trigger BufWrite for others
         }
     )
@@ -118,88 +190,18 @@ local function register_events()
     vim.api.nvim_create_autocmd({ "FocusGained" }, {
         group = group,
         desc = "auspicious-autosave focus",
-        callback = on_focus_gained,
+        callback = m.on_focus_gained,
         nested = true,
     })
 end
 
 ---@param buf? integer
 ---@return integer buf not 0
-local function real_buf(buf)
+function m.real_buf(buf)
     if buf == nil or buf == 0 then
         buf = vim.api.nvim_get_current_buf()
     end
     return buf
 end
-
-M = { -- public interface
-
-    setup = function()
-        -- controls CursorHold and CursorHoldI events
-        -- (idle time before they are triggered in milliseconds)
-        -- might want to check that it's not set to a longer value again later
-        vim.opt.updatetime = 500
-
-        -- checks if files have changed on disk, especially on FocusGained
-        vim.opt.autoread = true
-
-        -- in case anything slips by the events
-        -- (will write when windows change buffers in various ways)
-        vim.opt.autowrite = true
-        vim.opt.autowriteall = true
-
-        register_events()
-    end,
-
-    ---@param buf? integer
-    ---@return boolean
-    enabled = function(buf)
-        buf = real_buf(buf)
-        return enableds[buf]
-    end,
-
-    ---@param v boolean
-    ---@param buf? integer
-    set = function(v, buf)
-        buf = real_buf(buf)
-        enableds[buf] = v
-    end,
-
-    ---@param buf? integer
-    enable = function(buf)
-        M.set(true, buf)
-    end,
-
-    ---@param buf? integer
-    disable = function(buf)
-        M.set(false, buf)
-    end,
-
-    ---@param buf? integer
-    ---@return boolean
-    toggle = function(buf)
-        buf = real_buf(buf)
-        enableds[buf] = not enableds[buf]
-        return enableds[buf]
-    end,
-
-    ---@alias State "readonly" | "locked" | "autosave" |  "modified" | "saved" | "disappeared"
-    -- readonly     read-only file, no autosaving
-    -- locked       locked file (the file might still be writeable), no autosaving
-    -- autosave     normal autosave operation
-    -- modified     no autosaving, but the file has been modified
-    -- saved        no autosaving, the file has been saved and not changed since
-    -- disappeared  used to autosave, but then the file disappeared
-    --              wont autosave anymore, as that would recreate a file that was potentially removed on purpose
-    --              a manual :w or :bd can resolve the situation
-
-    ---@param buf? integer
-    ---@return State
-    state = function(buf)
-        buf = real_buf(buf)
-        update_state(buf)
-        return states[buf]
-    end,
-}
 
 return M
